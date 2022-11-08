fff91eb22d5966ef5b1378983a762cc6cd9392fb
Juliano Ribeiro de Souza Maciel <juliano.maciel@faculdadefacec.edu.br>
2022-11-08 20:06:55 -0300

    Implementações em sala

---
+++ Revisão.Quatro.Trimestre/Produto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revisão.Quatro.Trimestre
+{
+    public class Produto
+    {
+        public string Descricao { get; private set; }
+        public decimal Preco { get; private set; }
+        public int EAN { get; private set; }
+
+        //encapsulamento
+        public Produto(string descricao,
+            decimal preco, int ean)
+        {
+            Descricao = descricao;
+            Preco = preco;
+            EAN = ean;
+        }
+
+        //override sobre escrita de um método
+        //polimorfismo
+        public override string ToString()
+        {
+            return EAN + " " + Descricao + " " + Preco;
+        }
+    }
+}
---
+++ Revisão.Quatro.Trimestre/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Revisão.Quatro.Trimestre
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int menu = 1;
+
+            #region do while
+
+            //com o do while meu algoritmo
+            //sera executado ao menos uma vez
+            //do
+            //{
+            //    Console.WriteLine("ESCOLHA UMA OPÇÃO: ");
+            //    Console.WriteLine("1 - Cadastrar Produto");
+            //    Console.WriteLine("0 - Sair");
+
+            //    switch (menu)
+            //    {
+            //        case 1:
+            //            break;
+            //        default:
+            //            Console.WriteLine("Opção inválida!");
+            //            break;
+            //    }
+
+            //    menu = int.Parse(Console.ReadLine());
+            //} while (menu != 0);
+
+            #endregion do while
+
+            #region while
+
+            while (menu != 0)
+            {
+                Console.WriteLine("ESCOLHA UMA OPÇÃO: ");
+                Console.WriteLine("1 - Cadastrar Produto");
+                Console.WriteLine("0 - Sair");
+
+                menu = int.Parse(Console.ReadLine());
+
+                switch (menu)
+                {
+                    case 1:
+                        Produto produtoUm = new Produto(
+                            "COCA-COLA 350ml", 2.99m, 10001);
+                        
+                        // modificador private set não permite que essa
+                        // alteração ocorra
+                        //produtoUm.Descricao = "GUARANÁ ANTARTICA";
+
+                        Console.WriteLine(produtoUm.ToString());
+
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida!");
+                        break;
+                }
+
+            }
+
+            #endregion while
+
+            Console.WriteLine("Até mais!");
+        }
+    }
+}
